5757e076d737407d384864e1e821047a101dd68c
Elakilic <berfinela877@gmail.com>
2023-09-04 16:16:09 +0300

    Patron.cs oluşturuldu.

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -119,25 +119,7 @@ namespace MyProject
 
     
 
-    public class Patron
-    {
-        public int MusteriId { get; set; } //Müşteri özelliklerini tanımlamak için  
-        public string Ad { get; set; }
-        public string Adres { get; set; }
-        public int TelNo { get; set; }
-        public static void OduncAlinanlar(List<Book> books) 
-        {
-            Console.WriteLine("-------------------------------------------");
-            foreach (var book in books) //Ödünç alınan kitapları listeler
-            {
-                if (book.Durum == "Ödünç Alındı")
-                {
-                    Console.WriteLine(book.Baslik);
-
-                }
-            }
-        }
-    }
+    
 
     public class Library 
     {
